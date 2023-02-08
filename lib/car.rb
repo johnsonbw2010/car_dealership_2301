4da3f044550043d78ac79bf15e25b8ff983d6de8
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-08 12:16:05 -0500

    remove extra methods

--- lib/car.rb
+++ lib/car.rb
@@ -1,21 +1,12 @@
 class Car
   attr_reader :make, :model, :monthly_payment, :loan_length, :color
   def initialize(make_model, monthly_payment, loan_length)
-    @make = make_model
-    @model = make_model
+    mm = make_model.split(" ")
+    @make = "#{mm.first}"
+    @model = "#{mm.last}"
     @monthly_payment = monthly_payment
     @loan_length = loan_length
     @color = nil
-  end
-
-  def make
-    make = @make.split(" ")
-    @make = "#{make.first}"
-  end
-
-  def model
-    model = @model.split(" ")
-    @model = "#{model.last}"
   end
 
   def total_cost
--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -4,7 +4,7 @@ require './lib/car'
 RSpec.describe Car do
   it 'exists' do
     car = Car.new("Ford Mustang", 1500, 36)
-
+    
     expect(car).to be_a Car
   end
 
