35e066306a6489262e7dc469d0c56eebf1f555de
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-08 12:57:39 -0500

    finalize refactoring

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -38,11 +38,26 @@ class Dealership
     @inventory.each do |car|
       total_value << car.total_cost
     end
-    total_value.sum
+    "$#{total_value.sum.to_s.scan(/.{1,3}/).join(',')}"
   end
 
   def details
     value = self.total_value
-    p "Total value is #{value}, and the dealership is located at #{self.address}"
+    return "Total value is #{value}, and the dealership is located at #{self.address}"
+  end
+
+  def average_price_of_car
+    avg = []
+    @inventory.each do |car|
+      avg << car.total_cost
+    end
+    average_cost = "$#{(avg.sum).div(avg.count)}"
+    average_cost.scan(/.{1,3}/).join(',')
+  end
+
+  def cars_sorted_by_price
+    @inventory.sort_by do |car|
+      car.total_cost
+    end
   end
 end
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -52,8 +52,7 @@ RSpec.describe Dealership do
     dealership.add_car(car_3)
     dealership.add_car(car_4)
     expect(dealership.has_inventory?).to eq(true)
-    expect(dealership.total_value).to eq (156000)
-    require 'pry'; binding.pry
+    expect(dealership.total_value).to eq ("$156,000")
   end
 
   it 'can list details of dealership' do
@@ -66,6 +65,31 @@ RSpec.describe Dealership do
     dealership.add_car(car_2)
     dealership.add_car(car_3)
     dealership.add_car(car_4)
-    expect(dealership.details).to eq("Total value is 156000, and the dealership is located at 123 Main Street")
+    expect(dealership.details).to eq("Total value is $156,000, and the dealership is located at 123 Main Street")
+  end
+
+  it 'can find average cost of inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    car_3 = Car.new("Toyota Tercel", 500, 48)
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+    expect(dealership.average_price_of_car).to eq("$39,000")
+  end
+
+  it 'can sort by cost' do
+  dealership = Dealership.new("Acme Auto", "123 Main Street")
+  car_1 = Car.new("Ford Mustang", 1500, 36)
+  car_2 = Car.new("Toyota Prius", 1000, 48)
+  car_3 = Car.new("Toyota Tercel", 500, 48)
+  car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+  dealership.add_car(car_1)
+  dealership.add_car(car_2)
+  dealership.add_car(car_3)
+  dealership.add_car(car_4)
   end
 end
